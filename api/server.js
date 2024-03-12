ed8aed274d2c8481b92436f0b2792ad2d53d3fd9
Camden Hess <camden.hess12@gmail.com>
2024-03-12 15:32:19 -0600

    finished module project

--- api/server.js
+++ api/server.js
@@ -70,8 +70,17 @@ server.put('/api/users/:id', (req, res) => {
                     message: 'Please provide name and bio for the user'
                 })
             } else {
-                const updatedUser = User.update(req.params.id, req.body)
-                res.status(200).json(updatedUser)
+                User.update(req.params.id, req.body)
+                    .then(updatedUser => {
+                        res.status(200).json(updatedUser)
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            message: 'error editting user',
+                            err: err.message,
+                            stack: err.stack
+                        })
+                    })
             }
         })
         .catch(err => {
